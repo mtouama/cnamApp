91c89c428d59b79c3a319e5f487b538e8788d442
emmamau <emmanuel.maurice@gmail.com>
2024-03-29 21:26:42 +0100

    Update bootstrap.php
    Signed-off-by: emmamau <emmanuel.maurice@gmail.com>

--- deploy/bootstrap.php
+++ deploy/bootstrap.php
@@ -6,8 +6,7 @@
 	$isDevMode = true;
 	$config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
 	$conn = array(
-	'host' => 'dpg-co3hao6d3nmc739csiag-a',
-
+	'host' => 'dpg-co3hao6d3nmc739csiag-a.oregon-postgres.render.com',
 	'driver' => 'pdo_pgsql',
 	'user' => 'cnambdd_user',
 	'password' => 'oZvEBkIR2EMUPuyOQkSeAYUA9yIeG1os',
