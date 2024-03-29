340c393399f433d4b29f60f97bd3b5e71f4fb90e
Emmanuel MAURICE <emmanuel.maurice@gmail.com>
2024-03-29 20:14:22 +0000

    bdd

--- deploy/bootstrap.php
+++ deploy/bootstrap.php
@@ -6,12 +6,12 @@
 	$isDevMode = true;
 	$config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
 	$conn = array(
-	'host' => 'dpg-clvf4ida73kc73bp3kfg-a.oregon-postgres.render.com',
+	'host' => 'dpg-co3hao6d3nmc739csiag-a',
 
 	'driver' => 'pdo_pgsql',
-	'user' => 'cnam_db_akkt_user',
-	'password' => 'I8TW2jVgiSyQDguxH8De6kWjgJ6VjoR4',
-	'dbname' => 'cnam_db_akkt',
+	'user' => 'cnambdd_user',
+	'password' => 'oZvEBkIR2EMUPuyOQkSeAYUA9yIeG1os',
+	'dbname' => 'cnambdd',
 	'port' => '5432'
 	);
 
