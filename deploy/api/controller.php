0b6798a6053ae22dc1ef7ce2ca7495cbc305b2db
emmamau <emmanuel.maurice@gmail.com>
2024-05-20 08:47:39 +0200

    Update controller.php
    Signed-off-by: emmamau <emmanuel.maurice@gmail.com>

--- deploy/api/controller.php
+++ deploy/api/controller.php
@@ -38,7 +38,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 	// API Nécessitant un Jwt valide
 	function getCatalogue (Request $request, Response $response, $args) {
-	    $flux = '[{"titre":"linux","ref":"001","prix":"20"},{"titre":"java","ref":"002","prix":"21"},{"titre":"windows","ref":"003","prix":"22"},{"titre":"angular","ref":"004","prix":"23"},{"titre":"unix","ref":"005","prix":"25"},{"titre":"javascript","ref":"006","prix":"19"},{"titre":"html","ref":"007","prix":"15"},{"titre":"css","ref":"008","prix":"10"}]';
+	    $flux = '[{"titre":"linux","ref":"001","prix":"20"},{"titre":"java","ref":"002","prix":"21"},{"titre":"windows","ref":"003","prix":"22"},{"titre":"angular 17","ref":"004","prix":"23"},{"titre":"unix","ref":"005","prix":"25"},{"titre":"javascript","ref":"006","prix":"19"},{"titre":"html","ref":"007","prix":"15"},{"titre":"css","ref":"008","prix":"10"}]';
 	    $data = json_decode($flux, true); 
 	    
 	    $response->getBody()->write(json_encode($data));
